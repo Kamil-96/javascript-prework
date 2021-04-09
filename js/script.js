984a2363d86416255a56ff71a7ae1d663b7d311b
Kamil Put <kamil.put@interia.pl>
2021-04-09 08:27:06 +0200

    Change playGame value, remove console.log

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ function playGame(playerInput){
     clearMessages('');
     
     function getMoveName(argMoveId){
-        console.log('wywołano getMoveName');
         
         if(argMoveId == 1){
             return 'kamień';
@@ -14,8 +13,6 @@ function playGame(playerInput){
     }
 
     function displayResult(argComputerMove, argPlayerMove) {
-        console.log('wywołano displayResult');
-        console.log('moves:', argComputerMove, argPlayerMove);
         printMessage('Zagrałem: ' + argComputerMove + ', a Ty: ' + argPlayerMove);
         
         if(argComputerMove == 'kamień' && argPlayerMove == 'papier') {
@@ -30,28 +27,18 @@ function playGame(playerInput){
             printMessage('Ja wygrywam!');
         }
     }
-
     let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-    console.log('Wylosowana liczba to: ' + randomNumber);
-
     let computerMove = getMoveName(randomNumber);
-
-    console.log('Gracz wpisał: ' + playerInput);
-
     let playerMove = getMoveName(playerInput);
-
     displayResult(computerMove, playerMove);
-
-    console.log(getMoveName('2'));
 }
 
 document.getElementById('play-rock').addEventListener('click', function(){
-    playGame(argMoveId=1);
+    playGame(1);
 });
 document.getElementById('play-paper').addEventListener('click',  function(){
-    playGame(argMoveId=2);
+    playGame(2);
 });
 document.getElementById('play-scissors').addEventListener('click', function(){
-    playGame(argMoveId=3);
+    playGame(3);
 });
